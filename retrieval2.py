26f0335cea84d26afd83a4cadba74eaa21cde55d
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-05-27 23:29:18 -0700

    Cleaned up code.

--- retrieval2.py
+++ retrieval2.py
@@ -1,13 +1,8 @@
+# imports
 import sys
 import json
-# from tkinter.tix import INTEGER 
-import requests
 import json #
-import os#
-import re#
 import math#
-import lxml#
-import pprint #
 import nltk
 import time
 from nltk.corpus import stopwords
@@ -18,24 +13,16 @@ from bs4 import BeautifulSoup#
 from collections import defaultdict
 from nltk.tokenize import word_tokenize
 
-
-# Global variables
+# global variables
 stemmer = SnowballStemmer(language="english")
-doc_ids = None
-doc_ids_path = "/mnt/c/users/paolo/code/UCI/CS121/CS-121-Assignment-3/indexes/doc_id.txt" # Update the path to doc_id.txt, if necessary
 urls_path = "/mnt/c/users/paolo/code/UCI/CS121/CS-121-Assignment-3/urls.json"
 NUM_DOCUMENTS = 55393
-
-with open(doc_ids_path, "r") as text: 
-    line = text.readline()
-    doc_ids = eval(line)
-total_docs = len(doc_ids)
-
 with open(urls_path, "r") as json_file:
     urls = json.load(json_file)
 
-
 def get_top_urls(scores, top=5):
+    """Gets the top urls based on the scores for the given query"""
+
     scores = sorted(scores.items(), key=lambda key_value:key_value[1], reverse=True)
     # print(scores[:top])
     top_urls = list() # front of the list is the #1 ranked URL
@@ -44,13 +31,13 @@ def get_top_urls(scores, top=5):
     
     return top_urls[:top]
 
-
 def get_posting(input, exact):
+    """Gets the posting list based on the query input"""
+
     edited = input[len(exact) + 4:-3] # removes the word, beginning bracket, ending bracket, and newline from string
     find = edited.split(",") #splits everything into tuples in list
     posting = {int(str_pair.split(":")[0]):float(str_pair.split(":")[1]) for str_pair in find} # convert posting string to a posting dictionary with doc id as key and tf as value
     return posting # return posting
-
 
 def get_scores(input):
     """Using the lnc.ltc to ranks scores"""
@@ -74,10 +61,10 @@ def get_scores(input):
             query_idf = NUM_DOCUMENTS / math.log(len(posting)) if math.log(len(posting)) != 0 else 0 # idf for word 
         except:
             query_idf = 0
-
         query_tfidf = round(query_tf_wt * query_idf, 4) 
         query_vector[word] = query_tfidf
 
+        # Place term frequencies for each document
         for doc_id, tf in posting.items():
             document_vectors[doc_id][word] = tf
 
@@ -90,7 +77,6 @@ def get_scores(input):
         for word in doc_vector.keys():
             normalized_document_vectors[doc_id][word] = document_vectors[doc_id][word]/doc_vector_length if doc_vector_length > 0 else 0
 
-    
     # Calculate cosine scores by performing dot product of each query vector to each document vector
     for doc_id, doc_vector in normalized_document_vectors.items():
         
@@ -103,7 +89,6 @@ def get_scores(input):
     # print(normalized_document_vectors)
     return scores
 
-
 def parse_index(letter, word):
     # index_file = r"C:\Users\srb71\Documents\GitHub\CS-121-Assignment-3\indexes\index" + letter + ".txt"   SHOB'S path
     index_file = "/mnt/c/users/paolo/code/UCI/CS121/CS-121-Assignment-3/indexes/index" + letter + ".txt"
@@ -112,7 +97,6 @@ def parse_index(letter, word):
         exact = "\"" + word + "\""
         for entry in readfile:
             if exact in entry:
-                # print(entry)
                 return get_posting(entry, exact) # return posting if found
         
         return {} # returns empty posting if not found in inverted index
